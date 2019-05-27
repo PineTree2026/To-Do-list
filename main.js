e2b37f32f19e4e182d841fa4da88bcda9254a8b3
Akitaka Matsuse <38484726+pinetreee@users.noreply.github.com>
2019-05-27 13:44:52 +0900

    Update JavaScript

--- main.js
+++ main.js
@@ -1,4 +1,21 @@
-function createElement() {
+const list = document.getElementsByTagName("li");
+console.log(list);
+for (let i = 0; i < list.length; i++) {
+    list[i].addEventListener('click', function () {
+        alert();
+    }, false);
+}
+
+// Create and add new "li" element
+function newElement() {
     const li = document.createElement("li");
-    document.body.ul.appendChild(li);
+    const inputText = document.getElementById("to-do-input").value;
+    const t = document.createTextNode(inputText);
+    li.appendChild(t);
+    if (inputText === '') {
+        alert('Write something!')
+    } else {
+        document.getElementById("to-do-ul").appendChild(li);
+    }
+    document.getElementById("to-do-input").value = '';
 }
