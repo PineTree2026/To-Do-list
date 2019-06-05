b03faa96b12e47423f00f3def256fa67cc69a5c7
Akitaka Matsuse <38484726+pinetreee@users.noreply.github.com>
2019-06-05 22:31:36 +0900

    Add delete button

--- main.js
+++ main.js
@@ -1,21 +1,46 @@
-const list = document.getElementsByTagName("li");
-console.log(list);
-for (let i = 0; i < list.length; i++) {
-    list[i].addEventListener('click', function () {
-        alert();
+// Create delete button
+const li = document.getElementsByTagName('li');
+for (let i = 0; i < li.length; i++) {
+    const deleteButton = document.createElement('span');
+    const text =  document.createTextNode('×');
+    deleteButton.appendChild(text);
+    deleteButton.classList.add('delete');
+    li[i].appendChild(deleteButton);
+}
+
+// Delete 'li' elemtent
+const deleteList = document.getElementsByClassName('delete');
+for (let i = 0; i < deleteList.length; i ++) {
+    deleteList[i].addEventListener('click', () => {
+        const targetTag = deleteList[i].parentElement;
+        targetTag.parentElement.removeChild(targetTag);
     }, false);
 }
 
+// Toggle check
+const unorderedList = document.getElementById('to-do-ul');
+unorderedList.addEventListener('click', (event) => {
+    if (event.target.tagName === 'LI') {
+        event.target.classList.toggle('checked');
+    }
+}, false);
+
 // Create and add new "li" element
 function newElement() {
-    const li = document.createElement("li");
-    const inputText = document.getElementById("to-do-input").value;
+    const li = document.createElement('li');
+    const inputText = document.getElementById('to-do-input').value;
     const t = document.createTextNode(inputText);
     li.appendChild(t);
     if (inputText === '') {
         alert('Write something!')
     } else {
-        document.getElementById("to-do-ul").appendChild(li);
+        document.getElementById('to-do-ul').appendChild(li);
     }
-    document.getElementById("to-do-input").value = '';
+    document.getElementById('to-do-input').value = '';
+
+    const deleteButton = document.createElement('span');
+    const text =  document.createTextNode('×');
+    deleteButton.appendChild(text);
+    deleteButton.classList.add('delete');
+    li.appendChild(deleteButton);
 }
